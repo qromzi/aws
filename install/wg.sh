99b6909dcfe2546142ba544ff77b58893b46784e
qromzi <64263258+qromzi@users.noreply.github.com>
2021-11-28 10:42:54 +0800

    Update wg.sh
    wget -O addwg "https://raw.githubusercontent.com/${GitUser}/qromzi/main/tambah/addwg.sh"
    wget -O delwg "https://raw.githubusercontent.com/${GitUser}/qromzi/main/hapus/delwg.sh"
    wget -O cekwg "https://raw.githubusercontent.com/${GitUser}/qromzi/main/cekwg.sh"
    wget -O xp-wg "https://raw.githubusercontent.com/${GitUser}/qromzi/main/ctorxp-wg.sh"
    wget -O renewwg "https://raw.githubusercontent.com/${GitUser}/qromzi/main/renewwg.sh"
    
    adding /aws/ directory

--- install/wg.sh
+++ install/wg.sh
@@ -90,11 +90,11 @@ WG_RUNNING=$?
 
 # Tambahan
 cd /usr/bin
-wget -O addwg "https://raw.githubusercontent.com/${GitUser}/qromzi/main/tambah/addwg.sh"
-wget -O delwg "https://raw.githubusercontent.com/${GitUser}/qromzi/main/hapus/delwg.sh"
-wget -O cekwg "https://raw.githubusercontent.com/${GitUser}/qromzi/main/cekwg.sh"
-wget -O xp-wg "https://raw.githubusercontent.com/${GitUser}/qromzi/main/xp-wg.sh"
-wget -O renewwg "https://raw.githubusercontent.com/${GitUser}/qromzi/main/renewwg.sh"
+wget -O addwg "https://raw.githubusercontent.com/${GitUser}/qromzi/aws/main/tambah/addwg.sh"
+wget -O delwg "https://raw.githubusercontent.com/${GitUser}/qromzi/aws/main/hapus/delwg.sh"
+wget -O cekwg "https://raw.githubusercontent.com/${GitUser}/qromzi/aws/main/cekwg.sh"
+wget -O xp-wg "https://raw.githubusercontent.com/${GitUser}/qromzi/aws/main/xp-wg.sh"
+wget -O renewwg "https://raw.githubusercontent.com/${GitUser}/qromzi/aws/main/renewwg.sh"
 chmod +x addwg
 chmod +x delwg
 chmod +x cekwg
